17efab11bd33e21ad174e53ca031f3376eb28382
josueyat <josueyat318@gmail.com>
2021-03-30 16:53:38 +0800

    Clever Trickery updated

--- src/Backend/frontendData/getPortStatus.tsx
+++ src/Backend/frontendData/getPortStatus.tsx
@@ -24,7 +24,6 @@ createData('scanme.nmap.org', '5.41 seconds', 'GSFAFBMYjjmXkHqoIykQH','139', 'tc
 createData('scanme.nmap.org', '5.41 seconds', 'GSFAFBMYjjmXkHqoIykQH','445', 'tcp', 'filtered', 'microsoft-ds', 'no-response'),
 // ===================== END of scanme.nmap.org =================================
 
-<<<<<<< HEAD
 // ===================== Target: neopets.com ================================
 createData('neopets.com', '24.04 seconds', 'kCCNTSfyNbTtiqPfuOHre','80', 'tcp', 'open', 'http', 'syn-ack'),
 // ===================== END of neopets.com =================================
@@ -65,32 +64,5 @@ createData('scanme.nmap.org', '7.20 seconds', 'gPYHfIIAMoKvKixWsTnvu','80', 'tcp
 createData('scanme.nmap.org', '7.20 seconds', 'gPYHfIIAMoKvKixWsTnvu','135', 'tcp', 'filtered', 'msrpc', 'no-response'),
 createData('scanme.nmap.org', '7.20 seconds', 'gPYHfIIAMoKvKixWsTnvu','139', 'tcp', 'filtered', 'netbios-ssn', 'no-response'),
 createData('scanme.nmap.org', '7.20 seconds', 'gPYHfIIAMoKvKixWsTnvu','445', 'tcp', 'filtered', 'microsoft-ds', 'no-response'),
-=======
-// ===================== Target: 192.168.86.35 ================================
-createData('192.168.86.35', '2.08 seconds', 'VTzPKyfshdhjkPnuaIOBC','80', 'tcp', 'open', 'http', 'syn-ack'),
-createData('192.168.86.35', '2.08 seconds', 'VTzPKyfshdhjkPnuaIOBC','135', 'tcp', 'open', 'msrpc', 'syn-ack'),
-createData('192.168.86.35', '2.08 seconds', 'VTzPKyfshdhjkPnuaIOBC','139', 'tcp', 'open', 'netbios-ssn', 'syn-ack'),
-createData('192.168.86.35', '2.08 seconds', 'VTzPKyfshdhjkPnuaIOBC','443', 'tcp', 'open', 'https', 'syn-ack'),
-createData('192.168.86.35', '2.08 seconds', 'VTzPKyfshdhjkPnuaIOBC','445', 'tcp', 'open', 'microsoft-ds', 'syn-ack'),
-createData('192.168.86.35', '2.08 seconds', 'VTzPKyfshdhjkPnuaIOBC','5357', 'tcp', 'open', 'wsdapi', 'syn-ack'),
-// ===================== END of 192.168.86.35 =================================
-
-// ===================== Target: scanme.nmap.org ================================
-createData('scanme.nmap.org', '5.16 seconds', 'SfqDTehOLKhStOtHZrnLX','22', 'tcp', 'open', 'ssh', 'syn-ack'),
-createData('scanme.nmap.org', '5.16 seconds', 'SfqDTehOLKhStOtHZrnLX','25', 'tcp', 'filtered', 'smtp', 'no-response'),
-createData('scanme.nmap.org', '5.16 seconds', 'SfqDTehOLKhStOtHZrnLX','80', 'tcp', 'open', 'http', 'syn-ack'),
-createData('scanme.nmap.org', '5.16 seconds', 'SfqDTehOLKhStOtHZrnLX','135', 'tcp', 'filtered', 'msrpc', 'no-response'),
-createData('scanme.nmap.org', '5.16 seconds', 'SfqDTehOLKhStOtHZrnLX','139', 'tcp', 'filtered', 'netbios-ssn', 'no-response'),
-createData('scanme.nmap.org', '5.16 seconds', 'SfqDTehOLKhStOtHZrnLX','445', 'tcp', 'filtered', 'microsoft-ds', 'no-response'),
-// ===================== END of scanme.nmap.org =================================
-
-// ===================== Target: scanme.nmap.org ================================
-createData('scanme.nmap.org', '3.50 seconds', 'SNorErFqtmhspkLmxXgqo','22', 'tcp', 'open', 'ssh', 'syn-ack'),
-createData('scanme.nmap.org', '3.50 seconds', 'SNorErFqtmhspkLmxXgqo','25', 'tcp', 'filtered', 'smtp', 'no-response'),
-createData('scanme.nmap.org', '3.50 seconds', 'SNorErFqtmhspkLmxXgqo','80', 'tcp', 'open', 'http', 'syn-ack'),
-createData('scanme.nmap.org', '3.50 seconds', 'SNorErFqtmhspkLmxXgqo','135', 'tcp', 'filtered', 'msrpc', 'no-response'),
-createData('scanme.nmap.org', '3.50 seconds', 'SNorErFqtmhspkLmxXgqo','139', 'tcp', 'filtered', 'netbios-ssn', 'no-response'),
-createData('scanme.nmap.org', '3.50 seconds', 'SNorErFqtmhspkLmxXgqo','445', 'tcp', 'filtered', 'microsoft-ds', 'no-response'),
->>>>>>> 7448ad96e07759606c11b4537ee724d179b53376
 // ===================== END of scanme.nmap.org =================================
 ]
